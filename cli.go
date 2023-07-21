f958a8762b4e1134794c83a8940fac5295c61d96
Chris <nttriet9a4@gmail.com>
2023-07-21 19:02:54 +0700

    feat: cli

---
+++ cli.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type CLI struct {
+	bc *Blockchain
+}
+
+func (cli *CLI) printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println(" addblock -data BLOCK_DATA - add a block to the blockchain")
+	fmt.Println(" printchain - print all the blocks of the blockchain")
+}
+
+func (cli *CLI) validateArgs() {
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		os.Exit(1)
+	}
+}
+func (cli *CLI) Run() {
+	cli.validateArgs()
+
+	addBlockCmd := flag.NewFlagSet("addblock", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+
+	addBlockData := addBlockCmd.String("data", "", "Block data")
+
+	switch os.Args[1] {
+	case "addblock":
+		addBlockCmd.Parse(os.Args[2:])
+	case "printchain":
+		printChainCmd.Parse(os.Args[2:])
+	default:
+		cli.printUsage()
+		os.Exit(1)
+	}
+
+	if addBlockCmd.Parsed() {
+		if *addBlockData == "" {
+			addBlockCmd.Usage()
+			os.Exit(1)
+		}
+		cli.addBlock(*addBlockData)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
+}
+
+func (cli *CLI) addBlock(data string) {
+	cli.bc.AddBlock(data)
+	fmt.Println("A new block is Added")
+}
+
+func (cli *CLI) printChain() {
+	bci := cli.bc.Iterator()
+
+	fmt.Printf("Querying blockchain data:\n")
+	for {
+		b := bci.Next()
+		if b == nil {
+			fmt.Println("Ended")
+			break
+		}
+		fmt.Printf("Prev hash of block: %x\n", b.PrevBlockHash)
+		fmt.Printf("Data:  %s\n", b.Data)
+		fmt.Printf("Block Hash: %x\n", b.Hash)
+		pow := NewProofOfWork(b)
+		fmt.Printf("IsValid: %s \n\n", strconv.FormatBool(pow.Validate()))
+	}
+}
--- main.go
+++ main.go
@@ -1,37 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	fmt.Println("Creating a new blockchain ...")
-
 	bc := NewBlockchain()
+	defer bc.db.Close()
 
-	bc.AddBlock("Send 1 BTC to Alan")
-	bc.AddBlock("Send 2 BTC to Ivan")
-
-	bci := bc.Iterator()
-
-	var b *Block
-	b = bci.Next()
-	for {
-		fmt.Printf("Prev hash of block: %x\n", b.PrevBlockHash)
-		fmt.Printf("Data:  %s\n", b.Data)
-		fmt.Printf("Block Hash: %x\n", b.Hash)
-		b = bci.Next()
-		if b == nil {
-			break
-		}
-	}
-	// 	while (b != nil) {
-	//
-	// }
-	// for index, b := range bc.blocks {
-	// 	// Validate my chain
-	// 	pow := NewProofOfWork(b)
-	// 	fmt.Printf("Pow validation: %s\n", strconv.FormatBool(pow.Validate()))
-	// 	fmt.Println()
-	// }
+	cli := CLI{bc}
+	cli.Run()
 }
